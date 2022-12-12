505797ec24fbd1125552441476325106e23dd53c
Othon A. Menezes <othonam@live.com>
2022-12-12 09:56:22 -0300

    excluding unnecessary projects

--- Minimal.Api/Minimal.Infraestructure/Repositories/Base/IRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Minimal.Infrastructure.Repositories.Base
-{
-    public interface IRepository<T>
-    {
-        public Task<T> PostAsync(T entity);
-
-        public Task<IEnumerable<T>> GetAllAsync();
-    }
-}
